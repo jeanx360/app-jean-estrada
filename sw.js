031142e3388e318a929720dcff0222ca50ec8555
jeanx360 <jean.easy.conection.lan.house@gmail.com>
2026-07-22 13:44:42 -0300

    Add log message for disabled service worker

--- sw.js
+++ sw.js
@@ -3,3 +3,4 @@
 // ============================================
 // Este arquivo está vazio para desativar o cache.
 // Quando tudo estiver funcionando, podemos reativá-lo.
+console.log('⚠️ Service Worker desativado para testes.');
